67ce659c9ad4eb31f32b29f0424c3b9d71d93b33
Alex Liang <alex@Alexs-MacBook-Air.local>
2023-05-21 15:04:22 +1200

    added about page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import SearchBar from "./components/SearchBar";
+import AboutPage from "./components/AboutPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <h1>Weather App</h1>
-      <SearchBar />
+      <Router>
+        <NavBar />
+        <h1>Weather App V2</h1>
+        <Routes>
+          <Route path="/" element={<SearchBar />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>This is a remastered version of a weather app I previously made.</p>
+      <p>
+        You can check out the previous verion{" "}
+        <a
+          href="https://alux444.github.io/weather-api-project/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          here
+        </a>{" "}
+        and some of my other work on my website{" "}
+        <a href="https://alux444.github.io" target="_blank" rel="noreferrer">
+          here
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
